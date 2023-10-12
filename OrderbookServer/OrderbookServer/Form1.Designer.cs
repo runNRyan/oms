3ecf29c31120801e6214c7f589773f6f92f9a881
Lyu.gram <uk910415@gmail.com>
2023-10-13 04:35:25 +0900

    server working

--- OrderbookServer/OrderbookServer/Form1.Designer.cs
+++ OrderbookServer/OrderbookServer/Form1.Designer.cs
@@ -36,6 +36,9 @@
             btnStart = new Button();
             btnStop = new Button();
             lBClients = new ListBox();
+            label4 = new Label();
+            label5 = new Label();
+            lBLog = new ListBox();
             SuspendLayout();
             // 
             // label1
@@ -43,7 +46,7 @@
             label1.AutoSize = true;
             label1.Dock = DockStyle.Right;
             label1.Font = new Font("Arial", 20F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(501, 0);
+            label1.Location = new Point(467, 0);
             label1.Name = "label1";
             label1.Size = new Size(126, 32);
             label1.TabIndex = 0;
@@ -94,6 +97,7 @@
             btnStart.TabIndex = 5;
             btnStart.Text = "Start";
             btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
             // 
             // btnStop
             // 
@@ -104,23 +108,57 @@
             btnStop.TabIndex = 6;
             btnStop.Text = "Stop";
             btnStop.UseVisualStyleBackColor = true;
+            btnStop.Click += btnStop_Click;
             // 
             // lBClients
             // 
             lBClients.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
             lBClients.FormattingEnabled = true;
             lBClients.ItemHeight = 15;
-            lBClients.Location = new Point(21, 245);
+            lBClients.Location = new Point(21, 275);
             lBClients.Name = "lBClients";
-            lBClients.Size = new Size(217, 334);
+            lBClients.Size = new Size(217, 304);
             lBClients.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Arial", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(21, 253);
+            label4.Name = "label4";
+            label4.Size = new Size(47, 16);
+            label4.TabIndex = 9;
+            label4.Text = "Clients";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Arial", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(259, 65);
+            label5.Name = "label5";
+            label5.Size = new Size(34, 16);
+            label5.TabIndex = 10;
+            label5.Text = "LOG";
+            // 
+            // lBLog
+            // 
+            lBLog.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            lBLog.FormattingEnabled = true;
+            lBLog.ItemHeight = 15;
+            lBLog.Location = new Point(259, 95);
+            lBLog.Name = "lBLog";
+            lBLog.Size = new Size(316, 484);
+            lBLog.TabIndex = 11;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.GradientInactiveCaption;
-            ClientSize = new Size(627, 592);
+            ClientSize = new Size(593, 592);
+            Controls.Add(lBLog);
+            Controls.Add(label5);
+            Controls.Add(label4);
             Controls.Add(lBClients);
             Controls.Add(btnStop);
             Controls.Add(btnStart);
@@ -145,5 +183,8 @@
         private Button btnStart;
         private Button btnStop;
         private ListBox lBClients;
+        private Label label4;
+        private Label label5;
+        private ListBox lBLog;
     }
 }
--- OrderbookServer/OrderbookServer/Form1.cs
+++ OrderbookServer/OrderbookServer/Form1.cs
@@ -1,11 +1,78 @@
+using OrderbookLib.Communication;
+using OrderbookLib.Events;
+using OrderbookLib.Models;
+using System.Net;
+
 namespace OrderbookServer
 {
     public partial class Form1 : Form
     {
+        private CmServer _server;
+
+        private DTOHub CreateNewStateDTOHub(DTOHub hub, TcpState state)
+        {
+            return new DTOHub
+            {
+                UserId = hub.UserId,
+                State = state,
+            };
+        }
+        private void AddClientMessageList(DTOHub hub)
+        {
+            string message = hub.State switch
+            {
+                TcpState.Connect => $"≮ 立加 ≮ {hub} ≮",
+                TcpState.Disconnect => $"≮ 立加 辆丰 ≮ {hub} ≮",
+                _ => $"{hub}: Types {hub.Types}, State {hub.State}"
+            };
+            lBLog.Items.Add(message);
+        }
+
+        private void Connected(object? sender, CmEventArgs e)
+        {
+            var hub = CreateNewStateDTOHub(e.Hub, TcpState.Connect);
+
+            lBClients.Items.Add(e.Hub);
+            AddClientMessageList(hub);
+        }
+        private void Disconnected(object? sender, CmEventArgs e)
+        {
+            var hub = CreateNewStateDTOHub(e.Hub, TcpState.Disconnect);
+
+            lBClients.Items.Remove(e.Hub);
+            AddClientMessageList(hub);
+        }
+        private void Received(object? sender, CmEventArgs e)
+        {
+            AddClientMessageList(e.Hub);
+        }
+        private void RunningStateChanged(bool isRunning)
+        {
+            btnStart.Enabled = !isRunning;
+            btnStop.Enabled = isRunning;
+        }
+
         public Form1()
         {
             InitializeComponent();
+
+            _server = new CmServer(IPAddress.Parse("127.0.0.1"), 8080);
+            _server.Connected += Connected;
+            _server.Disconnected += Disconnected;
+            _server.Received += Received;
+            _server.RunningStateChanged += RunningStateChanged;
         }
 
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            _ = _server.StartAsync();
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+
+            _server.Stop();
+        }
     }
 }
